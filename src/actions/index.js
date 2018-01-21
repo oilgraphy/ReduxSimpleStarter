5c38229ad56ca5c147005fc0e6e774559dfc0894
Linsong Wang <wangl@Linsongs-MacBook-Pro.local>
2018-01-21 01:45:50 -0800

    search bar

---
+++ src/actions/index.js
@@ -0,0 +1,2 @@
+const API_KEY = '0c0e9bb3157c806898767e3afa7af8d4'
+
---
+++ src/containers/search_bar.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+class SearchBar extends Component {
+  constructor(props) {
+  	super(props);
+
+  	this.state = { term: '' };
+  	this.onInputChange = this.onInputChange.bind(this);
+  	//this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+
+  onInputChange(event) {
+  	this.setState({term: event.target.value});
+  }
+
+  onFormSubmit(event) {
+  	event.preventDefault();
+
+  	// we need to fetch weather data
+  }
+
+  render() {
+    return (
+      <form className='input-group' onSubmit={this.onFormSubmit}>
+        <input
+          placeholder='get 5-day forecast in your favorite cities'
+          className='form-control'
+          value={this.state.term}
+          onChange={this.onInputChange} />
+        <span className='input-group-btn'>
+          <button type='submit' className='btn btn-secondary'>Submit</button>
+        </span>
+      </form>
+    );
+  }
+
+}
+
+export default SearchBar;
